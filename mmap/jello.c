0a1bb7b6f55f8404964db2e13d2004e707fffb1b
Quentin Ågren <quentin.agren@gmail.com>
2025-09-24 18:43:06 +0200

    Add fancy macro

--- mmap/jello.c
+++ mmap/jello.c
@@ -1,19 +1,26 @@
 #include <sys/mman.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
 
+#define errexit(func) printf("ERROR: " func ": %s\n", strerror(errno)), exit(1)
+
 // CS:APP -- Exercise 9.14
-int main() {
+void main() {
   char *ptr;
   int fd;
-  fd = open("hello.txt", O_RDWR);
+
+  if ((fd = open("hello.txt", O_RDWR)) == -1) {
+    errexit("open");
+  }
+
   if ((ptr = mmap(NULL, 1, PROT_WRITE, MAP_SHARED, fd, 0)) == MAP_FAILED) {
-    printf("Error: %d %s\n", errno, strerror(errno));
-    return errno;
+    errexit("mmap");
   };
+
   close(fd);
   *ptr = 'J';
 }
--- mmap/test.sh
+++ mmap/test.sh
@@ -1,5 +1,9 @@
 echo -n "Before: "
 echo Hello | tee hello.txt
-./jello
-echo -n "After: "
-cat hello.txt
+if ./jello; then
+  echo -n "After: "
+  cat hello.txt
+else
+  echo "FAIL: jello"
+fi
+echo Hello > hello.txt
