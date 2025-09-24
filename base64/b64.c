10625988ea715a5894cc1069892e729721e080bf
Quentin Ågren <quentin.agren@gmail.com>
2025-09-24 07:22:21 +0200

    Handle incomplete chunks in buffered encoding

--- base64/b64.c
+++ base64/b64.c
@@ -1,10 +1,18 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
 
+// Base64 alphabet
 static const char *base = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-const int BUFSIZE = 4096;
+
+// Input buffer size
+#define BUFSIZE 4096
+
+// Groups of 3 bytes translate to groups of 4 bytes.
+#define IN_CHUNK_SIZE 3
+#define OUT_CHUNK_SIZE 4
 
 void encode_chunk(unsigned char *in, int len, unsigned char *out) {
 
@@ -38,33 +46,57 @@ void encode_unbuffered() {
   unsigned char in[3];
   unsigned char out[4];
   int len;
-  while (len = read(0, in, 3)) {
+  while (len = read(STDIN_FILENO, in, 3)) {
     encode_chunk(in, len, out);
     write(1, out, 4);
     in[0] = in[1] = in[2] = 0;
   }
 }
 
-int encode_buffered() {
-  unsigned char in[BUFSIZE];
-  unsigned char out[4 * (BUFSIZE / 3 + 1)];
-  int len, chunk_len, in_offset, out_offset;
+void err_exit(char *msg) {
+  printf("ERROR: %s: %s", msg, strerror(errno));
+  exit(1);
+}
 
-  while (len = read(0, in, BUFSIZE)) {
-    if (len == -1) {
-      printf("read error: %s", strerror(errno));
-      return 1;
-    }
+void encode_buffered() {
+  unsigned char in[BUFSIZE];
+  // 3*k + r input bytes give 4*(k +1) output bytes
+  unsigned char out[OUT_CHUNK_SIZE * (BUFSIZE / IN_CHUNK_SIZE + 1)];
+  ssize_t r_len;
+  int in_len, rem, in_offset, out_offset;
+
+  while (true) {
+    r_len = read(STDIN_FILENO, in + rem, BUFSIZE - rem);
+
+    // Read error
+    if (r_len == -1)
+      err_exit("read");
+
+    // Input buffer length is what remains from the previous round,
+    // plus what has just been read.
+    if ((in_len = rem + r_len) == 0)
+      break;
 
     in_offset = out_offset = 0;
-    while (in_offset < len) {
-      chunk_len = len - in_offset;
-      encode_chunk(in + in_offset, chunk_len, out + out_offset);
-      in_offset+=3;
-      out_offset+=4;
+    rem = in_len;
+    while (rem > 2) {
+      encode_chunk(in + in_offset, 3, out + out_offset);
+      in_offset += 3;
+      out_offset += 4;
+      rem -= 3;
     }
+
+    // Only encode the last incomplete chunk if input is exhausted
+    if ((r_len == 0) && (rem > 0))
+      encode_chunk(in + in_offset, rem, out + out_offset);
+
     // TODO: Error handling
-    write(1, out, out_offset) == -1)
+    write(STDOUT_FILENO, out, out_offset);
+
+    // Place trailing partial chunk at the beginning of in
+    for (int i = 0; i < rem; i++)
+      in[i] = in[in_offset + i];
+  }
 }
 
 int main() {
