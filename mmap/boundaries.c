9da8c691e6901816930f51d3c032cba75561cd68
Quentin Ågren <quentin.agren@gmail.com>
2025-09-27 18:54:50 +0200

    Observe core dump

--- mmap/boundaries.c
+++ mmap/boundaries.c
@@ -4,24 +4,38 @@
 // $ lsblk -l
 #define DISK_SECTOR_SIZE 512
 // $ getconf PAGE_SIZE
-#define PAGE_SIZE 4069
-// File consists of 1024 ASCII 1 characters. See Makefile
-#define MAPPED_FILE "page.txt"
-#define MAPPED_FILE_SIZE 1024
-#define MAPPED_LENGTH 512
+#define PAGE_SIZE 4096
+// File consists of 2048 ASCII 1 characters. See Makefile
+#define MAPPED_FILE "halfpage.txt"
+#define MAPPED_FILE_SIZE 2048
+
 
 int main(void) {
   char *ptr;
   int fd;
-  char nl = 0xa;
+  char tmp;
 
   fd = open(MAPPED_FILE, O_RDONLY);
-  ptr = mmap(NULL, MAPPED_LENGTH, PROT_READ, MAP_PRIVATE, fd, 0);
-  // within requested mapped range: outputs 1
+
+  /* Map half the file
+     |##......|   Requested mapped size
+     |1111....|   File size
+     |vvvvvvvv|   Actual mapped size (rounded up)
+     |11110000|   Seen
+   */
+  ptr = mmap(NULL, MAPPED_FILE_SIZE / 2, PROT_READ, MAP_PRIVATE, fd, 0);
   write(STDOUT_FILENO, ptr, 8);
-  // outside of requested range, but whithin file range: still outputs 1
-  write(STDOUT_FILENO, ptr + MAPPED_LENGTH, 8);
-  // outside of file range, but whithin a page size: outputs 0x0
+  write(STDOUT_FILENO, ptr + MAPPED_FILE_SIZE / 2, 8);
   write(STDOUT_FILENO, ptr + MAPPED_FILE_SIZE, 8);
-  return 0;
+
+  /* Map two pages
+     |########|########|   Requested map size
+     |1111....|........|   File size
+     |vvvvvvvv|vvvvvvvv|
+     |11110000|BBBBBBBB|   Seen; B=SIGBUS
+   */
+  fd = open(MAPPED_FILE, O_RDWR);
+  ptr = mmap(NULL, 2 * PAGE_SIZE, PROT_WRITE, MAP_SHARED, fd, 0);
+  tmp = *(ptr + PAGE_SIZE);
+  return 1;
 }
