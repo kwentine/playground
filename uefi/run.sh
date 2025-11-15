2ee473e6ec8593e26b772e87dc46d76b349da6fb
Quentin Ågren <quentin.agren@gmail.com>
2025-11-15 19:49:15 +0100

    First UEFI app

--- uefi/run.sh
+++ uefi/run.sh
@@ -2,4 +2,4 @@
 # See: https://github.com/tianocore/tianocore.github.io/wiki/How-to-run-OVMF
 qemu-system-x86_64 -bios OVMF_CODE.fd \
                    -net none \
-                   -drive file=fat:rw:disk-contents,format=raw
+                   -drive file=fat:rw:esp,format=raw
