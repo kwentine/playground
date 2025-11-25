27b5a649ca6e3dbd66ccff85531cc1ca525c0a88
Quentin Ågren <quentin.agren@gmail.com>
2025-11-25 19:06:32 +0100

    Clone of keylog in rust

--- tty/keylog/src/main.rs
+++ tty/keylog/src/main.rs
@@ -1,11 +1,14 @@
-use std::io::{self, Read};
+use termion::raw::IntoRawMode;
+use std::io::{self, Read, Write, stdout};
 use std::ascii;
 
 fn main() -> io::Result<()> {
-    println!("Press 'q' to quit.");
+    println!("Logging keystrokes. To quit, press 'q'.");
+    println!("{:>5} {:>5} {:>10} {:>5}", "Hex", "Oct", "Bin", "Dec");
+    let mut stdout = stdout().into_raw_mode()?;
     for b in io::stdin().bytes() {
         let c = b?;
-        println!("{:08b} {:02x} {:>3}  {}", c, c, c, ascii::escape_default(c));
+        write!(stdout, "{:5x} {:5o} {:10.08b} {:5} {:4}\r\n", c, c, c, c, ascii::escape_default(c))?;
         if c == b'q' {
             break
         }
