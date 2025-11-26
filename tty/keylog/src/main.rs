e2fb6af4619ee91fef8bbf4ab66e27001da34bde
Quentin Ågren <quentin.agren@gmail.com>
2025-11-26 19:10:23 +0100

    Set terminal in raw mode using rustix

--- tty/keylog/src/main.rs
+++ tty/keylog/src/main.rs
@@ -1,14 +1,18 @@
-use termion::raw::IntoRawMode;
+//use termion::raw::IntoRawMode;
+
 use std::io::{self, Read, Write, stdout};
 use std::ascii;
+
+pub mod keylog;
 
 fn main() -> io::Result<()> {
     println!("Logging keystrokes. To quit, press 'q'.");
     println!("{:>5} {:>5} {:>10} {:>5}", "Hex", "Oct", "Bin", "Dec");
-    let mut stdout = stdout().into_raw_mode()?;
+//    let mut stdout = stdout().into_raw_mode()?;
+    keylog::make_raw();
     for b in io::stdin().bytes() {
         let c = b?;
-        write!(stdout, "{:5x} {:5o} {:10.08b} {:5} {:4}\r\n", c, c, c, c, ascii::escape_default(c))?;
+        write!(stdout(), "{:5x} {:5o} {:10.08b} {:5} {:4}\r\n", c, c, c, c, ascii::escape_default(c))?;
         if c == b'q' {
             break
         }
