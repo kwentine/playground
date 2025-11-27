0657dca6c0e8f6bbd7c099be9a894e5f46f2fa5a
Quentin Ågren <quentin.agren@gmail.com>
2025-11-27 19:10:57 +0100

    Slowly getting there...

--- tty/keylog/src/main.rs
+++ tty/keylog/src/main.rs
@@ -1,21 +1,27 @@
-//use termion::raw::IntoRawMode;
-
-use std::io::{self, Read, Write, stdout};
+use rustix::termios::{tcgetattr, tcsetattr, OptionalActions};
+use std::io::{self, Write, Read};
 use std::ascii;
-
-pub mod keylog;
 
 fn main() -> io::Result<()> {
     println!("Logging keystrokes. To quit, press 'q'.");
     println!("{:>5} {:>5} {:>10} {:>5}", "Hex", "Oct", "Bin", "Dec");
-//    let mut stdout = stdout().into_raw_mode()?;
-    keylog::make_raw();
+    let mut stdout = io::stdout();
+    let saved_termios = tcgetattr(&stdout).expect("Error!");
+    make_raw(&stdout)?;
     for b in io::stdin().bytes() {
         let c = b?;
-        write!(stdout(), "{:5x} {:5o} {:10.08b} {:5} {:4}\r\n", c, c, c, c, ascii::escape_default(c))?;
+        write!(stdout, "{:5x} {:5o} {:10.08b} {:5} {:4}\r\n", c, c, c, c, ascii::escape_default(c))?;
         if c == b'q' {
             break
         }
     }
+    tcsetattr(stdout, OptionalActions::Flush, &saved_termios)?;
     Ok(())
+}
+
+pub fn make_raw(stdout: &io::Stdout) -> rustix::io::Result<()> {
+    let mut t = tcgetattr(stdout)?;
+    t.make_raw();
+    tcsetattr(stdout, OptionalActions::Flush, &t)?;
+    return Ok(());
 }
