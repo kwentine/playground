04501cb0d31038252b48ec9c2cd4efb1856d79b0
Quentin Ågren <quentin.agren@gmail.com>
2025-11-29 10:07:39 +0100

    Do not write into buffer if no bytes were read

--- tty/lagom/lagom.c
+++ tty/lagom/lagom.c
@@ -6,17 +6,22 @@
 
 char scroll_buffer[SCROLL_ROWS][MAXLINE];
 
+// Read the next input line, truncated to MAXLINE
 int read_line(int fd, char buff[MAXLINE]) {
-  int i;
+  int i = 0;
   char c;
-  while ((c = getchar()) != EOF && c != '\n' && i < MAXLINE - 1)
-    buff[i++] = c;
+  while ((c = getchar()) != EOF && c != '\n') {
+    if (i < MAXLINE - 1)
+      buff[i++] = c;
+  }
+  if (c == EOF)
+    return 0;
   buff[i++] = '\n';
   return i;
 }
 
 int main() {
   int n;
-  n = read_line(0, scroll_buffer[0]);
-  write(1, scroll_buffer[0], n);
+  while (n = read_line(0, scroll_buffer[0]))
+    write(1, scroll_buffer[0], n);
 }
