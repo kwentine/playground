7c2fa7b272758db5ea784fcc8abe11f83f2f1b0a
Quentin Ågren <quentin.agren@gmail.com>
2025-11-29 12:02:05 +0100

    First popcorn time

--- tty/lagom/lagom.c
+++ tty/lagom/lagom.c
@@ -3,6 +3,7 @@
 
 #define MAXLINE 1024
 #define SCROLL_ROWS 32
+#define DISPLAY_ROWS 16
 
 char scroll_buffer[SCROLL_ROWS][MAXLINE];
 
@@ -22,10 +23,38 @@ void reset(int row) {
   printf("\x1b[2J\x1b[%dd", row);
 }
 
+void enter_cbreak() {}
+
+void draw(int row, int last_line, char sbuff[][MAXLINE]) {
+  int i = 0, j = 0;
+  reset(row);
+  // Find where to start from
+  j = (last_line - DISPLAY_ROWS + SCROLL_ROWS) % SCROLL_ROWS;
+  if (*sbuff[j] == '\0')
+    j = 0;
+  for (i = 0; i < DISPLAY_ROWS; i++) {
+    if (*sbuff[j] == '\0') {
+      printf("~\n");
+    } else {
+      printf(" %s", sbuff[j]);
+    }
+    j = (j + 1) % SCROLL_ROWS;
+  }
+}
 
 int main() {
-  int n;
-  reset(10);
-  while (n = read_line(scroll_buffer[0]))
-    printf("%s", scroll_buffer[0]);
+  int i, start_line, c;
+  // Initialize scroll buffer
+  for (i = 0; i < SCROLL_ROWS; i++) {
+    scroll_buffer[i][0] = '\0';
+  }
+  i = 0;
+  while (read_line(scroll_buffer[i])){
+    draw(10, i, scroll_buffer);
+    sleep(1);
+    i = (i + 1) % SCROLL_ROWS;
+  }
+  // Wait for sigint
+  while (true)
+    sleep(1);
 }
