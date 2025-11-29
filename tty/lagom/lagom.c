ad74abe91b28700e15e1bf6e1511e85f0c3d143c
Quentin Ågren <quentin.agren@gmail.com>
2025-11-29 16:53:35 +0100

    Cleanup

--- tty/lagom/lagom.c
+++ tty/lagom/lagom.c
@@ -4,8 +4,12 @@
 #define MAXLINE 1024
 #define SCROLL_ROWS 32
 #define DISPLAY_ROWS 16
+#define MARGIN_TOP 10
+#define next(idx) (idx + 1) % SCROLL_ROWS
 
-char scroll_buffer[SCROLL_ROWS][MAXLINE];
+static char scroll_buffer[SCROLL_ROWS][MAXLINE];
+static int read_idx = 0;
+static int write_idx = 0;
 
 // Read the next input line, truncated to MAXLINE
 int read_line(char buff[MAXLINE]) {
@@ -23,38 +27,41 @@ void reset(int row) {
   printf("\x1b[2J\x1b[%dd", row);
 }
 
-void enter_cbreak() {}
-
-void draw(int row, int last_line, char sbuff[][MAXLINE]) {
-  int i = 0, j = 0;
+void draw(int row) {
+  int i = 0, j = read_idx;
   reset(row);
-  // Find where to start from
-  j = (last_line - DISPLAY_ROWS + SCROLL_ROWS) % SCROLL_ROWS;
-  if (*sbuff[j] == '\0')
-    j = 0;
-  for (i = 0; i < DISPLAY_ROWS; i++) {
-    if (*sbuff[j] == '\0') {
+  for (i = 0; i < DISPLAY_ROWS; i++, j = (j + 1) % SCROLL_ROWS) {
+    if (*scroll_buffer[j] == '\0')
       printf("~\n");
-    } else {
-      printf(" %s", sbuff[j]);
-    }
-    j = (j + 1) % SCROLL_ROWS;
+    else
+      printf(" %s", scroll_buffer[j]);
+  }
+}
+
+void init() {
+  int i;
+  for (i = 0; i < SCROLL_ROWS; i++)
+    read_line(scroll_buffer[i]);
+  read_idx = write_idx = 0;
+}
+
+void refill() {
+  while (write_idx != read_idx) {
+    read_line(scroll_buffer[write_idx]);
+    write_idx = (write_idx + 1) % SCROLL_ROWS;
   }
 }
 
 int main() {
-  int i, start_line, c;
-  // Initialize scroll buffer
-  for (i = 0; i < SCROLL_ROWS; i++) {
-    scroll_buffer[i][0] = '\0';
-  }
-  i = 0;
-  while (read_line(scroll_buffer[i])){
-    draw(10, i, scroll_buffer);
+  int i, c;
+  init();
+  while (*scroll_buffer[read_idx] != '\0'){
+    draw(MARGIN_TOP);
     sleep(1);
-    i = (i + 1) % SCROLL_ROWS;
+    if ((read_idx + DISPLAY_ROWS) % SCROLL_ROWS == write_idx)
+      refill();
+    read_idx = (read_idx + 1) % SCROLL_ROWS;
   }
-  // Wait for sigint
   while (true)
     sleep(1);
 }
