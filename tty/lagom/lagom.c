203988c03ea847cb88bc025bc379e98a3ce13bab
Quentin Ågren <quentin.agren@gmail.com>
2025-11-29 10:38:29 +0100

    Ensure a valid C string is read

--- tty/lagom/lagom.c
+++ tty/lagom/lagom.c
@@ -7,21 +7,18 @@
 char scroll_buffer[SCROLL_ROWS][MAXLINE];
 
 // Read the next input line, truncated to MAXLINE
-int read_line(int fd, char buff[MAXLINE]) {
-  int i = 0;
-  char c;
-  while ((c = getchar()) != EOF && c != '\n') {
-    if (i < MAXLINE - 1)
+int read_line(char buff[MAXLINE]) {
+  int i = 0, c;
+  while ((c = getchar()) != EOF && c != '\n' && i < MAXLINE - 2)
       buff[i++] = c;
-  }
-  if (c == EOF)
-    return 0;
-  buff[i++] = '\n';
+  if (c == '\n')
+    buff[i++] = '\n';
+  buff[i] = '\0'
   return i;
 }
 
 int main() {
   int n;
-  while (n = read_line(0, scroll_buffer[0]))
-    write(1, scroll_buffer[0], n);
+  while (n = read_line(scroll_buffer[0]))
+    printf("%s\n", scroll_buffer[0]);
 }
